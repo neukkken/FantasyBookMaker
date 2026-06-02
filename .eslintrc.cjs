ad8e7361f4c5a0a906401e413f981d85d377047c
neukkken <santyagonarvaezns@gmail.com>
2026-06-02 15:47:53 -0500

    fix: corrección de ESLint y TS config para CI pipeline (v1.1.1)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,14 +4,46 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 module.exports = {
   extends: [
     'eslint:recommended',
-    'plugin:vue/vue3-recommended',
     '@electron-toolkit',
+    '@electron-toolkit/eslint-config-ts',
     '@electron-toolkit/eslint-config-ts/eslint-recommended',
-    '@vue/eslint-config-typescript/recommended',
-    '@vue/eslint-config-prettier'
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier'
   ],
+  plugins: ['@typescript-eslint'],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
   rules: {
-    'vue/require-default-prop': 'off',
-    'vue/multi-word-component-names': 'off'
-  }
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'react-hooks/set-state-in-effect': 'off',
+    'react-hooks/refs': 'off',
+    'react-hooks/globals': 'off',
+    'react-hooks/immutability': 'off',
+    'react-hooks/preserve-manual-memoization': 'off',
+    'react-hooks/purity': 'off',
+    'react/no-unescaped-entities': 'off',
+    'react/no-unknown-property': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'no-unused-vars': 'off',
+    'no-empty': 'warn',
+    'no-useless-escape': 'warn',
+    '@typescript-eslint/explicit-function-return-type': [
+      'warn',
+      { allowExpressions: true }
+    ]
+  },
+  overrides: [
+    {
+      files: ['*.js', '*.jsx', '*.cjs'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': 'off'
+      }
+    }
+  ]
 }
